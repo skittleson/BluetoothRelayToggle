8cf5d7e1812d3e30fe393ed7065ceef71fb666dc
Spencer Kittleson <spencerkittleson@hotmail.com>
2022-04-04 14:14:22 -0700

    init

---
+++ DoorGate.ino
@@ -0,0 +1,113 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+#define SERVICE_UUID        "2396a27a-45cd-11ec-81d3-0242ac130003"
+#define CHARACTERISTIC_UUID "2e08641e-45cd-11ec-81d3-0242ac130003"
+#define CHIP_CHARACTERISTIC_UUID "dd37f38f-ff65-45b1-a7b9-ca8f017a8ec0"
+uint32_t chipId = 0;
+int gateTrigger = 0;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+BLEServer* pServer = NULL;
+
+
+class RelayCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *relayCharacteristic) {
+      
+      // Get value from the callback service then if it true, set the gate trigger to true. otherwise set to default again. toggle logic.
+      std::string value = relayCharacteristic->getValue();
+      Serial.println(value.c_str());
+      if(value == "1"){
+        gateTrigger = 1;
+      } else {
+        relayCharacteristic->setValue(gateTrigger);
+      }
+    }
+};
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      BLEDevice::startAdvertising();
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  BLEDevice::init("DoorGate");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *relayCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+  BLECharacteristic *chirelayCharacteristic = pService->createCharacteristic(
+                                        CHIP_CHARACTERISTIC_UUID,
+                                        BLECharacteristic::PROPERTY_READ
+                                       );
+  relayCharacteristic->setCallbacks(new RelayCallbacks());
+  relayCharacteristic->setValue(gateTrigger);
+
+  // set chip id
+  for(int i=0; i<17; i=i+8) {
+	  chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
+	}
+  chirelayCharacteristic->setValue(chipId);
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  // pService->start();
+  // BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  // pAdvertising->addServiceUUID(SERVICE_UUID);
+  // pAdvertising->setScanResponse(true);
+  // pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  // pAdvertising->setMinPreferred(0x12);
+  // BLEDevice::startAdvertising();
+}
+
+void loop() {
+
+   // notify changed value
+    // if (deviceConnected) {
+    //     pCharacteristic->setValue((uint8_t*)&value, 4);
+    //     pCharacteristic->notify();
+    //     value++;
+    //     delay(10); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+    // }
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
+
+   // https://doc.riot-os.org/group__boards__esp32__mh-et-live-minikit.html
+  digitalWrite(LED_BUILTIN, LOW);
+  if(gateTrigger == 1) {
+    Serial.println("trigger");
+    digitalWrite(LED_BUILTIN, HIGH);
+    gateTrigger = 0;
+    delay(2000);
+  }
+}
